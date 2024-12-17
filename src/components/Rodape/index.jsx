71383f289deacdd8d287be039c173b524fd7a041
renatodavid <renatomdavid811@gmail.com>
2024-12-16 22:58:21 -0300

    subindo imagens!!

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Rodape = () => {
+  return (
+    <footer className="text-center bg-black text-bg-dark">
+      <p className="card-text py-3">
+        2025 <i className="bi bi-c-circle"></i> Everest
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
